f53e0a6d754744353ac2b732a4c5213e4eaee1b2
Artin-Etaati <artinetaati@gmail.com>
2026-09-17 12:39:35 -0700

    nothing

---
+++ main.java
@@ -0,0 +1,23 @@
+public class main {
+    public static void main(String[] args){
+
+        
+        // LinkedList l1 = new LinkedList();
+        // l1.add(2);
+        // l1.add(3);
+        // l1.printList();
+
+// ------------------------------------------------
+        // LinkedList[] array = new LinkedList[3];
+        // array[0] = new LinkedList();
+        // array[0].add(5);
+        // array[0].printList();
+
+// ------------------------------------------------
+
+
+
+        
+    
+    }
+}
