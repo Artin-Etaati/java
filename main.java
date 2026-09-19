e1f063cbc0307ed1ce18b61e8397b1b938183191
Artin-Etaati <161558173+Artin-Etaati@users.noreply.github.com>
2026-09-19 16:47:00 -0700

    Somne errors and loopholes have been eliminated

--- main.java
+++ main.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class main {
     public static void printMenu(){
-        System.out.println("\n To initialize Enter 1: \n" + "To create a child Enter 2: \n" + 
-        "To destroy a child Enter 3: \n" + " To quit Enter 4: \n");
+        System.out.println("""
+                           
+                            To initialize Enter 1: 
+                           To create a child Enter 2: 
+                           To destroy a child Enter 3: 
+                            To quit Enter 4: 
+                           """);
 }
     public static void main(String[] args){
 
@@ -26,62 +31,64 @@ while(input != 4){
         printMenu();
         input = scn.nextInt();
 
-    if(input == 1){  //intitialize the frist process
-        if(pcb[0] != null){
-            System.out.println("The first process already has been intialized.");
+    switch (input) {
+        case 1:
+            //intitialize the frist process
+            if(pcb[0] != null){
+                System.out.println("The first process already has been intialized.");
+            }
+            else if(pcb[0] == null){
+                pcb[0] = new Process(-1);  // The root
+                pcb[0].printParentID();
+                pcb[0].printChildList();
+            }   break;
+        case 2:
+            // creat child
+            
+            System.out.println("Enter the parent id: ");
+            int parent = scn.nextInt();
+            int childIndex = -1;
+            if(pcb[parent] == null){ // checks if the parent exists
+                System.out.println("This parentId is invalid. \n  Maybe try checking to see if you have initialized the first process.");
+                break;
+            } 
+            for ( int i = 1 ; i < pcb.length ; i++){
+                if(pcb[i] == null){
+                    childIndex = i;
+                    break;
+                }
+            }   if( childIndex == -1 ){
+                System.out.println("There is no space");
+                
+                printMenu();
+                input = scn.nextInt();
+            }
+            else{
+                
+                pcb[childIndex] = new Process(parent); // create a new child with a parent id
+                pcb[parent].addChildToParent(childIndex);  // adds the childId to the parent child id
+                
+                System.out.println("Here is the parent process information: ");
+                pcb[parent].printParentID();
+                pcb[parent].printChildList();
+                
+                System.out.println("Here is the child process infromation: ");
+                pcb[childIndex].printParentID();
+                pcb[childIndex].printChildList();
+            }   break;
+        case 4:
+            System.out.println("you have existed out of the program");
+            break;
+        default:
+            System.out.println("this command is not defined");
             printMenu();
             input = scn.nextInt();
-        }
-        else if(pcb[0] == null){
-            pcb[0] = new Process(-1);  // The root
-            pcb[0].printParentID();
-            pcb[0].printChildList();
-        }
-    }
-
-    else if(input == 2){  // creat child
-
-        System.out.println("Enter the parent id: ");
-        int parent = scn.nextInt();
-        int childIndex = -1;
-
-        for ( int i = 1 ; i < pcb.length ; i++){
-            if(pcb[i] == null){
-                childIndex = i;
-                break;
-            }
-        }
-
-        if( childIndex == -1 ){
-            System.out.println("There is no space");
-
-             printMenu();
-             input = scn.nextInt();
-        }
-        pcb[childIndex] = new Process(parent); // create a new child with a parent id
-        pcb[parent].addChildToParent(childIndex);  // adds the childId to the parent child id
-
-        System.out.println("Here is the parent process information: ");
-        pcb[parent].printParentID();
-        pcb[parent].printChildList();
-
-        System.out.println("Here is the child process infromation: ");
-        pcb[childIndex].printParentID();
-        pcb[childIndex].printChildList();
-    }
-
-
-
-
-    else{
-        System.out.println("this command is not defined");
-        printMenu();
-        input = scn.nextInt();
+            break;
     }
 
 
 }
-System.out.println("you have existed out of the program");
+
 
 
 
